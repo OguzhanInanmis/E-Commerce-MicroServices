d6a4154390add56cb49e8463a76b32fc3a2742a3
Oğuzhan İnanmış <inanmisoguzhan205@gmail.com>
2024-05-09 17:00:34 +0300

    Dapper ile discount microservice yapıldı.

---
+++ Services/Discount/E-Commerce.Discount/Context/DapperContext.cs
@@ -0,0 +1,27 @@
+﻿using E_Commerce.Discount.Entities;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace E_Commerce.Discount.Context
+{
+    public class DapperContext : DbContext
+    {
+        private readonly IConfiguration configuration;
+        private readonly string connectionString;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;initial catalog=E-CommerceDiscountDb;integrated security=true;TrustServerCertificate=True");
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+        public IDbConnection CreateConnection() => new SqlConnection(connectionString);
+    }
+}
---
+++ Services/Discount/E-Commerce.Discount/Services/IDiscountService.cs
@@ -0,0 +1,13 @@
+﻿using E_Commerce.Discount.Dtos;
+
+namespace E_Commerce.Discount.Services
+{
+    public interface IDiscountService
+    {
+        Task<List<ResultCouponDto>> GetAllCouponsAsync();
+        Task CreateCouponAsync(CreateCouponDto createCouponDto);
+        Task UpdateCouponAsync(UpdateCouponDto updateCouponDto);
+        Task DeleteCouponAsync(int couponId);
+        Task<ResultCouponDto> GetByIdCouponAsync(int couponId);
+    }
+}
